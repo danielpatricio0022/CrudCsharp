1c79d223422ba439754fbdba5d403a5c5788dfdf
danielpatricio0022 <hadukens@hotmail.com>
2024-09-26 19:45:05 -0300

    remove

--- CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
+++ CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e92af3788ad68576e178416f395e43d450d994")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5af3cf652655d9b0d33b44b6c01739af68ed736")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
